556139d2e08b3f96825f36c970db12e2077c7bae
cdewen <96959326+cdewen@users.noreply.github.com>
2023-03-30 13:37:49 -0500

    hides old SPDC key

--- src/getFriendData.js
+++ src/getFriendData.js
@@ -2,7 +2,6 @@ const buddyList = require('spotify-buddylist')
 const fs = require('fs')
 
 async function fetchData () {
- //const spDcCookie = 'AQCxQ5Xy5l8xjf64qhzQQtLRgpKncS8gx9XhkbgN9pI5X9coZxRIrmSoJpNWXq51x8Ua_64Z7J5HLqzbkwv4F_RO4y99KW8x41cChxUM5eUkNYmYRmMvPPEpryHIw5CwEotwikkytXIF2dedrJK-a5UdNIFoXufy'
 const spDcCookie = getSPDC()
 
  const { accessToken } = await buddyList.getWebAccessToken(spDcCookie)
